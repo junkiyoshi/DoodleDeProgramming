8f72c58970bde6b3168d75d85489afea7e73e4be
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2025-11-30 18:56:58 +0900

    Floral pattern. https://junkiyoshi.com/openframeworks20251130/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,7 +6,9 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openframeworks");
 
-	ofBackground(39);
+	ofBackground(239);
+	ofSetCircleResolution(60);
+	ofSetLineWidth(2);
 }
 
 //--------------------------------------------------------------
@@ -19,39 +21,47 @@ void ofApp::draw() {
 
 	ofTranslate(ofGetWindowSize() * 0.5);
 
-	int deg_span = 120;
+	int deg_span = 40;
 	int radius_span = 1;
+	int deg_len = 3;
 
-	for (int deg_len = 5; deg_len > 0; deg_len -= 1) {
+	ofFill();
+	ofSetColor(239, 39, 39);
+	ofDrawCircle(glm::vec2(), 320 + radius_span);
 
-		ofSetColor(ofMap(deg_len, 5, 1, 255, 0));
+	ofSetColor(0);
+	ofFill();
 
-		for (int deg = 0; deg < 360; deg += deg_span) {
+	for (int deg = 0; deg < 360; deg += deg_span) {
 
-			for (int radius = 5; radius < 720; radius += radius_span) {
+		for (int radius = 5; radius < 320; radius += radius_span) {
 
-				auto noise_deg = ofMap(ofNoise(radius * 0.0025 - ofGetFrameNum() * 0.01), 0, 1, 180, -180);
-				auto next_noise_deg = ofMap(ofNoise((radius + radius_span) * 0.0025 - ofGetFrameNum() * 0.01), 0, 1, 180, -180);
+			ofSetColor(radius < 50 ? 0 : ofMap(radius, 50, 320, 0, 239));
 
-				ofBeginShape();
-				ofVertex(glm::vec2(radius * cos((deg + noise_deg - deg_len) * DEG_TO_RAD), radius * sin((deg + noise_deg - deg_len) * DEG_TO_RAD)));
-				ofVertex(glm::vec2(radius * cos((deg + noise_deg + deg_len) * DEG_TO_RAD), radius * sin((deg + noise_deg + deg_len) * DEG_TO_RAD)));
-				ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg + deg_len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg + deg_len) * DEG_TO_RAD)));
-				ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg - deg_len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg - deg_len) * DEG_TO_RAD)));
-				ofEndShape(true);
+			auto noise_deg = ofGetFrameNum() * 2 + ofMap(ofNoise(radius * 0.0023 - ofGetFrameNum() * 0.0001), 0, 1, 180, -180);
+			auto next_noise_deg = ofGetFrameNum() * 2 + ofMap(ofNoise((radius + radius_span) * 0.0023 - ofGetFrameNum() * 0.0001), 0, 1, 180, -180);
 
-				noise_deg = ofMap(ofNoise(radius * 0.0025 - ofGetFrameNum() * 0.01), 0, 1, -180, 180);
-				next_noise_deg = ofMap(ofNoise((radius + radius_span) * 0.0025 - ofGetFrameNum() * 0.01), 0, 1, -180, 180);
+			ofBeginShape();
+			ofVertex(glm::vec2(radius * cos((deg + noise_deg - deg_len) * DEG_TO_RAD), radius * sin((deg + noise_deg - deg_len) * DEG_TO_RAD)));
+			ofVertex(glm::vec2(radius * cos((deg + noise_deg + deg_len) * DEG_TO_RAD), radius * sin((deg + noise_deg + deg_len) * DEG_TO_RAD)));
+			ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg + deg_len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg + deg_len) * DEG_TO_RAD)));
+			ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg - deg_len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg - deg_len) * DEG_TO_RAD)));
+			ofEndShape(true);
 
-				ofBeginShape();
-				ofVertex(glm::vec2(radius * cos((deg + noise_deg - deg_len) * DEG_TO_RAD), radius * sin((deg + noise_deg - deg_len) * DEG_TO_RAD)));
-				ofVertex(glm::vec2(radius * cos((deg + noise_deg + deg_len) * DEG_TO_RAD), radius * sin((deg + noise_deg + deg_len) * DEG_TO_RAD)));
-				ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg + deg_len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg + deg_len) * DEG_TO_RAD)));
-				ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg - deg_len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg - deg_len) * DEG_TO_RAD)));
-				ofEndShape(true);
-			}
+			noise_deg = ofMap(ofNoise(radius * 0.0023 - ofGetFrameNum() * 0.0001), 0, 1, -180, 180) - ofGetFrameNum() * 2;
+			next_noise_deg = ofMap(ofNoise((radius + radius_span) * 0.0023 - ofGetFrameNum() * 0.0001), 0, 1, -180, 180) - ofGetFrameNum() * 2;
+
+			ofBeginShape();
+			ofVertex(glm::vec2(radius * cos((deg + noise_deg - deg_len) * DEG_TO_RAD), radius * sin((deg + noise_deg - deg_len) * DEG_TO_RAD)));
+			ofVertex(glm::vec2(radius * cos((deg + noise_deg + deg_len) * DEG_TO_RAD), radius * sin((deg + noise_deg + deg_len) * DEG_TO_RAD)));
+			ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg + deg_len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg + deg_len) * DEG_TO_RAD)));
+			ofVertex(glm::vec2((radius + radius_span) * cos((deg + next_noise_deg - deg_len) * DEG_TO_RAD), (radius + radius_span) * sin((deg + next_noise_deg - deg_len) * DEG_TO_RAD)));
+			ofEndShape(true);
 		}
 	}
+
+	ofNoFill();
+	ofDrawCircle(glm::vec2(), 320 + radius_span);
 
 	/*
 	// ffmpeg -i img_%04d.jpg aaa.mp4
