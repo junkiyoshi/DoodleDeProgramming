9eada7b990a81fe79c943202c88fcd1fcd23cc1e
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2025-12-04 19:39:52 +0900

    Bar chart. https://junkiyoshi.com/openframeworks20251204/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,53 +4,55 @@
 void ofApp::setup() {
 
 	ofSetFrameRate(25);
-	ofSetWindowTitle("openFrameworks");
+	ofSetWindowTitle("openframeworks");
 
-	ofBackground(39);
+	ofBackground(239);
+	ofNoFill();
 	ofSetLineWidth(2);
-	ofEnableDepthTest();
-
-	this->frame.setMode(ofPrimitiveMode::OF_PRIMITIVE_LINES);
-
-	int span = 10;
-	for (int x = -120; x <= 120; x += span) {
-
-		for (int y = -120; y <= 120; y += span) {
-
-			for (int z = -120; z <= 120; z += span) {
-
-				this->location_list.push_back(glm::vec3(x, y, z));
-			}
-		}
-	}
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
 
-	ofSeedRandom(39);
-
-	this->face.clear();
-	this->frame.clear();
-
-	float size = 10;
-	for (int i = 0; i < this->location_list.size(); i++) {
-
-		this->setBoxToMesh(this->face, this->frame, this->location_list[i], size);
-	}
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
 
-	this->cam.begin();
-	ofRotateY(ofGetFrameNum() * 1.44);
+	ofTranslate(ofGetWindowSize() * 0.5);
 
-	this->face.draw();
-	this->frame.drawWireframe();
+	ofSeedRandom(39);
+	ofSetColor(39);
+	for (int deg = 90; deg < 360; deg += 180) {
 
-	this->cam.end();
+		ofPushMatrix();
+		ofRotate(deg);
 
+		auto base_location = glm::vec2(0, -10);
+		auto target_location = glm::vec2(0, -360);
+		auto distance = target_location - base_location;
+		auto len = glm::length(distance);
+		auto noise_seed = ofRandom(1000);
+
+		ofBeginShape();
+		for (int d = 0; d <= len; d += 1) {
+
+			auto location = base_location + glm::normalize(distance) * d;
+
+			auto gap = abs(len * 0.5 - d);
+			auto power = gap < len * 0.2 ? 1 : ofMap(gap, len * 0.2, len * 0.5, 1, 0);
+
+			auto noise_x = ofMap(ofNoise(noise_seed, location.x * 0.05, (((int)location.y / 10) * 10) * 0.02 + ofGetFrameNum() * 0.06), 0, 1, -350, 350);
+			location += glm::vec2(noise_x * power, 0);
+
+			ofVertex(location);
+		}
+		ofEndShape();
+
+		ofPopMatrix();
+	}
+
+	/*
 	// ffmpeg -i img_%04d.jpg aaa.mp4
 	int start = 250;
 	if (ofGetFrameNum() > start) {
@@ -65,83 +67,7 @@ void ofApp::draw() {
 			std::exit(1);
 		}
 	}
-}
-
-//--------------------------------------------------------------
-void ofApp::setBoxToMesh(ofMesh& face_target, ofMesh& frame_target, glm::vec3 location, float size) {
-
-	this->setBoxToMesh(face_target, frame_target, location, size, size, size);
-}
-
-//--------------------------------------------------------------
-void ofApp::setBoxToMesh(ofMesh& face_target, ofMesh& frame_target, glm::vec3 location, float height, float width, float depth) {
-
-	auto noise_value = ofNoise(glm::vec4(location.x * 0.01, location.z * 0.01, location.y * 0.01 + ofGetFrameNum() * 0.02, ofGetFrameNum() * 0.02));
-
-	if (noise_value < 0.45) { return; }
-	else if (noise_value < 0.5) { noise_value = ofMap(noise_value, 0.45, 0.5, 0, 1); }
-	else { noise_value = 1; }
-
-	int face_index = face_target.getNumVertices();
-	int frame_index = frame_target.getNumVertices();
-
-	vector<glm::vec3> vertices;
-	vertices.push_back(glm::vec3(width * -0.5, height * 0.5, depth * -0.5));
-	vertices.push_back(glm::vec3(width * 0.5, height * 0.5, depth * -0.5));
-	vertices.push_back(glm::vec3(width * 0.5, height * 0.5, depth * 0.5));
-	vertices.push_back(glm::vec3(width * -0.5, height * 0.5, depth * 0.5));
-
-	vertices.push_back(glm::vec3(width * -0.5, height * -0.5, depth * -0.5));
-	vertices.push_back(glm::vec3(width * 0.5, height * -0.5, depth * -0.5));
-	vertices.push_back(glm::vec3(width * 0.5, height * -0.5, depth * 0.5));
-	vertices.push_back(glm::vec3(width * -0.5, height * -0.5, depth * 0.5));
-
-	for (auto& vertex : vertices) {
-
-		face_target.addVertex(location + vertex * 0.99 * noise_value);
-		frame_target.addVertex(location + vertex * noise_value);
-	}
-
-	face_target.addIndex(face_index + 0); face_target.addIndex(face_index + 1); face_target.addIndex(face_index + 2);
-	face_target.addIndex(face_index + 0); face_target.addIndex(face_index + 2); face_target.addIndex(face_index + 3);
-
-	face_target.addIndex(face_index + 4); face_target.addIndex(face_index + 5); face_target.addIndex(face_index + 6);
-	face_target.addIndex(face_index + 4); face_target.addIndex(face_index + 6); face_target.addIndex(face_index + 7);
-
-	face_target.addIndex(face_index + 0); face_target.addIndex(face_index + 4); face_target.addIndex(face_index + 1);
-	face_target.addIndex(face_index + 4); face_target.addIndex(face_index + 5); face_target.addIndex(face_index + 1);
-
-	face_target.addIndex(face_index + 1); face_target.addIndex(face_index + 5); face_target.addIndex(face_index + 6);
-	face_target.addIndex(face_index + 6); face_target.addIndex(face_index + 2); face_target.addIndex(face_index + 1);
-
-	face_target.addIndex(face_index + 2); face_target.addIndex(face_index + 6); face_target.addIndex(face_index + 7);
-	face_target.addIndex(face_index + 7); face_target.addIndex(face_index + 3); face_target.addIndex(face_index + 2);
-
-	face_target.addIndex(face_index + 3); face_target.addIndex(face_index + 7); face_target.addIndex(face_index + 4);
-	face_target.addIndex(face_index + 4); face_target.addIndex(face_index + 0); face_target.addIndex(face_index + 3);
-
-	frame_target.addIndex(frame_index + 0); frame_target.addIndex(frame_index + 1);
-	frame_target.addIndex(frame_index + 1); frame_target.addIndex(frame_index + 2);
-	frame_target.addIndex(frame_index + 2); frame_target.addIndex(frame_index + 3);
-	frame_target.addIndex(frame_index + 3); frame_target.addIndex(frame_index + 0);
-
-	frame_target.addIndex(frame_index + 4); frame_target.addIndex(frame_index + 5);
-	frame_target.addIndex(frame_index + 5); frame_target.addIndex(frame_index + 6);
-	frame_target.addIndex(frame_index + 6); frame_target.addIndex(frame_index + 7);
-	frame_target.addIndex(frame_index + 7); frame_target.addIndex(frame_index + 4);
-
-	frame_target.addIndex(frame_index + 0); frame_target.addIndex(frame_index + 4);
-	frame_target.addIndex(frame_index + 1); frame_target.addIndex(frame_index + 5);
-	frame_target.addIndex(frame_index + 2); frame_target.addIndex(frame_index + 6);
-	frame_target.addIndex(frame_index + 3); frame_target.addIndex(frame_index + 7);
-
-	ofColor face_color = noise_value < 1 ? ofColor(239, 39, 39) : ofColor(39);
-	ofColor frame_color = noise_value < 1 ? ofColor(39) : ofColor(239, 39, 39);
-	for (int i = 0; i < 8; i++) {
-
-		face_target.addColor(face_color);
-		frame_target.addColor(frame_color);
-	}
+	*/
 }
 
 //--------------------------------------------------------------
