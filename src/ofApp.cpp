ecf5897484f1a6bbfc784cea6c50d9de0c579fe2
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-01-26 20:13:10 +0900

    Gradient text. https://junkiyoshi.com/openframeworks20260126/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,13 +6,11 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openFrameworks");
 
-	ofBackground(239);
+	ofBackground(39);
 	ofSetRectMode(ofRectMode::OF_RECTMODE_CENTER);
 
 	this->font.loadFont("fonts/msgothic.ttc", 28, true, true, true);
-	//this->word = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	this->word = "JUNKIYOSHI.COM ";
-	//this->word = "0123456789";
+	this->word = "junkiyoshi.com ";
 }
 
 //--------------------------------------------------------------
@@ -25,10 +23,14 @@ void ofApp::update() {
 void ofApp::draw() {
 
 	int span = 33;
-	for (int y = 2; y < ofGetWindowHeight() + 33; y += span) {
+	ofColor color;
+	for (int y = 0; y < ofGetWindowHeight() + 33; y += span) {
 
-		int x_start = ofMap(ofNoise(y * 0.01 - ofGetFrameNum() * 0.005), 0, 1, 0, 1000);
+		int x_start = ofMap(ofNoise(y * 0.1, ofGetFrameNum() * 0.005), 0, 1, 0, 1000);
+
 		for (int i = 0; i < this->word.size() * 2; i++) {
+
+			color.setHsb((int)(ofMap(y, 0, ofGetWindowHeight() + 33, 0, 255) + ofGetFrameNum() * 10) % 255, 100, 255);
 
 			auto x = (int)(x_start + i * span) % 1000 - 100;
 			auto location = glm::vec2(x, y);
@@ -39,23 +41,8 @@ void ofApp::draw() {
 			ofPath chara_path = this->font.getCharacterAsPoints(this->word[i % this->word.size()], true, false);
 			vector<ofPolyline> outline = chara_path.getOutline();
 
-			ofFill();
-			ofSetColor(39, 39, 239);
-			ofBeginShape();
-			for (int outline_index = 0; outline_index < outline.size(); outline_index++) {
-
-				ofNextContour(true);
-
-				auto vertices = outline[outline_index].getVertices();
-				for (auto& vertex : vertices) {
-
-					ofVertex(vertex);
-				}
-			}
-			ofEndShape(true);
-
 			ofNoFill();
-			ofSetColor(39);
+			ofSetColor(color);
 			ofBeginShape();
 			for (int outline_index = 0; outline_index < outline.size(); outline_index++) {
 
