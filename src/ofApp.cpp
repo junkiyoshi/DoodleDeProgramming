980136c9487e21c626c40ea0d4b7c780d72d380d
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2026-09-21 20:08:30 +0900

    Panels that form a ring. https://junkiyoshi.com/openframeworks20260921/

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,7 +6,7 @@ void ofApp::setup() {
 	ofSetFrameRate(25);
 	ofSetWindowTitle("openFrameworks");
 
-	ofBackground(239);
+	ofBackground(39);
 	ofEnableDepthTest();
 
 	this->line.setMode(ofPrimitiveMode::OF_PRIMITIVE_LINES);
@@ -20,49 +20,49 @@ void ofApp::update() {
 	this->face.clear();
 	this->line.clear();
 
-	float phi_deg_step = 0.25;
-	float theta_deg_step = 0.25;
-	float noise_gap = 2;
+	float phi_deg_step = 5;
+	float theta_deg_step = 5;
+	float noise_gap = 1;
 	float noise_step = 0.035;
-	float noise_threshold_1 = 0.47;
-	float noise_threshold_2 = 0.53;
+	float noise_threshold_1 = 0.45;
+	float noise_threshold_2 = 0.55;
 
-	for (float radius = 500; radius <= 550; radius += 25) {
+	for (float radius = 200; radius <= 300; radius += 5) {
 
-		float noise_radius = radius * 0.0075;
+		auto noise_radius = radius * 0.001;
 
-		for (float phi_deg = 0; phi_deg < 180; phi_deg += phi_deg_step) {
+		for (float phi_deg = 0; phi_deg < 360; phi_deg += phi_deg_step) {
 
-			for (float theta_deg = 30; theta_deg <= 150; theta_deg += theta_deg_step) {
+			for (float theta_deg = 60; theta_deg <= 120; theta_deg += theta_deg_step) {
 
 				auto noise_value = ofNoise(
-					cos(phi_deg * DEG_TO_RAD) * noise_gap,
-					sin(phi_deg * DEG_TO_RAD) * noise_gap,
-					cos(theta_deg * DEG_TO_RAD) * noise_gap,
-					noise_radius + ofGetFrameNum() * noise_step);
+					noise_radius + cos(phi_deg * DEG_TO_RAD) * noise_gap,
+					noise_radius + sin(phi_deg * DEG_TO_RAD) * noise_gap,
+					noise_radius + cos(theta_deg * DEG_TO_RAD) * noise_gap,
+					ofGetFrameNum() * noise_step);
 
 				if (noise_value < noise_threshold_1 || noise_value > noise_threshold_2) { continue; }
 
 				auto noise_value_1 = ofNoise(
-					cos(phi_deg * DEG_TO_RAD) * noise_gap,
-					sin(phi_deg * DEG_TO_RAD) * noise_gap,
-					cos((theta_deg - theta_deg_step) * DEG_TO_RAD) * noise_gap,
-					noise_radius + ofGetFrameNum() * noise_step);
+					noise_radius + cos(phi_deg * DEG_TO_RAD) * noise_gap,
+					noise_radius + sin(phi_deg * DEG_TO_RAD) * noise_gap,
+					noise_radius + cos((theta_deg - theta_deg_step) * DEG_TO_RAD) * noise_gap,
+					ofGetFrameNum() * noise_step);
 				auto noise_value_2 = ofNoise(
-					cos((phi_deg + phi_deg_step) * DEG_TO_RAD) * noise_gap,
-					sin((phi_deg + phi_deg_step) * DEG_TO_RAD) * noise_gap,
-					cos(theta_deg * DEG_TO_RAD) * noise_gap,
-					noise_radius + ofGetFrameNum() * noise_step);
+					noise_radius + cos((phi_deg + phi_deg_step) * DEG_TO_RAD) * noise_gap,
+					noise_radius + sin((phi_deg + phi_deg_step) * DEG_TO_RAD) * noise_gap,
+					noise_radius + cos(theta_deg * DEG_TO_RAD) * noise_gap,
+					ofGetFrameNum() * noise_step);
 				auto noise_value_3 = ofNoise(
-					cos((phi_deg - phi_deg_step) * DEG_TO_RAD) * noise_gap,
-					sin((phi_deg - phi_deg_step) * DEG_TO_RAD) * noise_gap,
-					cos(theta_deg * DEG_TO_RAD) * noise_gap,
-					noise_radius + ofGetFrameNum() * noise_step);
+					noise_radius + cos((phi_deg - phi_deg_step) * DEG_TO_RAD) * noise_gap,
+					noise_radius + sin((phi_deg - phi_deg_step) * DEG_TO_RAD) * noise_gap,
+					noise_radius + cos(theta_deg * DEG_TO_RAD) * noise_gap,
+					ofGetFrameNum() * noise_step);
 				auto noise_value_4 = ofNoise(
-					cos(phi_deg * DEG_TO_RAD) * noise_gap,
-					sin(phi_deg * DEG_TO_RAD) * noise_gap,
-					cos((theta_deg + theta_deg_step) * DEG_TO_RAD) * noise_gap,
-					noise_radius + ofGetFrameNum() * noise_step);
+					noise_radius + cos(phi_deg * DEG_TO_RAD) * noise_gap,
+					noise_radius + sin(phi_deg * DEG_TO_RAD) * noise_gap,
+					noise_radius + cos((theta_deg + theta_deg_step) * DEG_TO_RAD) * noise_gap,
+					ofGetFrameNum() * noise_step);
 
 				auto index = this->face.getNumVertices();
 				vector<glm::vec3> vertices;
@@ -89,7 +89,7 @@ void ofApp::update() {
 				this->face.addIndex(index + 0); this->face.addIndex(index + 1); this->face.addIndex(index + 3);
 				this->face.addIndex(index + 0); this->face.addIndex(index + 3); this->face.addIndex(index + 2);
 
-				if (noise_value_1 < noise_threshold_1 || noise_value_1 > noise_threshold_2) {
+				if (noise_value_1 < noise_threshold_1 || noise_value_1 > noise_threshold_2 || theta_deg == 60) {
 
 					this->line.addVertex(vertices[0]); this->line.addVertex(vertices[1]);
 					this->line.addIndex(this->line.getNumVertices() - 1); this->line.addIndex(this->line.getNumVertices() - 2);
@@ -107,7 +107,7 @@ void ofApp::update() {
 					this->line.addIndex(this->line.getNumVertices() - 1); this->line.addIndex(this->line.getNumVertices() - 2);
 				}
 
-				if (noise_value_4 < noise_threshold_1 || noise_value_4 > noise_threshold_2) {
+				if (noise_value_4 < noise_threshold_1 || noise_value_4 > noise_threshold_2 || theta_deg == 120) {
 
 					this->line.addVertex(vertices[2]); this->line.addVertex(vertices[3]);
 					this->line.addIndex(this->line.getNumVertices() - 1); this->line.addIndex(this->line.getNumVertices() - 2);
@@ -122,6 +122,7 @@ void ofApp::draw() {
 
 	this->cam.begin();
 	ofRotateX(270);
+	ofRotateZ(ofGetFrameNum() * 0.18);
 
 	ofSetColor(0);
 	this->face.draw();
